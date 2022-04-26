2bd01e52eacb7e264848e58bde1691e24b0bb38e
Tyler Henry <tyler.henry2@dcmail.ca>
2022-04-26 03:16:40 -0400

    Add project files.

---
+++ Tutorial5/Box.cs
@@ -0,0 +1,103 @@
+﻿//Name: Tyler Henry
+//Date: March 13, 2022
+//Title Tutorial 5
+//Description: Calcualtes volume of boxes in a list and then sorts then using overload operators
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeDemo
+{
+    class Box
+    {
+        //Variable declarations
+        private double length { get; set; }
+        private double width { get; set; }
+        private double height { get; set; }
+
+        /// <summary>
+        /// Public constructor
+        /// </summary>
+        /// <param name="m_length"></param>
+        /// <param name="m_width"></param>
+        /// <param name="m_height"></param>
+        public Box(double m_length, double m_width, double m_height)
+        {
+            this.length = m_length;
+            this.width = m_width;
+            this.height = m_height;
+        }
+
+        /// <summary>
+        /// Calculates volume of object
+        /// </summary>
+        /// <returns></returns>
+        public double getVolume()
+        {
+            return (length * width * height);
+        }
+
+        /// <summary>
+        /// Overloads >= to use get volume
+        /// </summary>
+        /// <param name="volA"></param>
+        /// <param name="volB"></param>
+        /// <returns></returns>
+        public static bool operator >=(Box volA, Box volB)
+        {
+            return volA.getVolume() >= volB.getVolume();
+        }
+        /// <summary>
+        /// Overloads <= to use get volume
+        /// </summary>
+        /// <param name="volA"></param>
+        /// <param name="volB"></param>
+        /// <returns></returns>
+        public static bool operator <=(Box volA, Box volB)
+        {
+            return volA.getVolume() <= volB.getVolume();
+        }
+
+        /// <summary>
+        /// Overloads == to use get volume
+        /// </summary>
+        /// <param name="volA"></param>
+        /// <param name="volB"></param>
+        /// <returns></returns>
+        public static bool operator ==(Box volA, Box volB)
+        {
+            return volA.getVolume() == volB.getVolume();
+        }
+
+        /// <summary>
+        /// Overloads != to use get volume
+        /// </summary>
+        /// <param name="volA"></param>
+        /// <param name="volB"></param>
+        /// <returns></returns>
+        public static bool operator !=(Box volA, Box volB)
+        {
+            return volA.getVolume() != volB.getVolume();
+        }
+
+        /// <summary>
+        /// output box volumes
+        /// </summary>
+        /// <param name="boxes"></param>
+        /// <returns></returns>
+        public string toString(IEnumerable<Box> boxes)
+        {
+            //Output each object in given list and return string
+            string outputString = "";
+            foreach (Box box in boxes)
+            {
+                outputString += box.getVolume();
+                outputString += ("\n");
+
+            }
+            return outputString;
+        }
+    }
+}
---
+++ Tutorial5/Program.cs
@@ -0,0 +1,56 @@
+﻿//Name: Tyler Henry
+//Date: March 13, 2022
+//Title Tutorial 5
+//Description: Calcualtes volume of boxes in a list and then sorts then using overload operators
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create list of boxes and object variable
+            var boxes = new List<Box>();
+            var box = new Box(1,1,1);
+
+           //add to list of boxes
+            boxes.Add(new Box(10, 23, 54));
+            boxes.Add(new Box(10, 2, 54));
+            boxes.Add(new Box(10, 23, 4));
+            boxes.Add(new Box(10, 3, 54));
+            boxes.Add(new Box(1, 23, 54));
+            boxes.Add(new Box(1, 23, 5));
+            boxes.Add(new Box(10, 3, 54));
+            boxes.Add(new Box(10, 23, 54));
+            boxes.Add(new Box(10, 23, 59));
+            boxes.Add(new Box(1, 1, 4));
+
+            //Sort and output list
+            boxes.Sort(sort);
+            Console.WriteLine(box.toString(boxes));        
+        }
+        /// <summary>
+        /// Sorts given list by volume
+        /// </summary>
+        /// <param name="boxes"></param>
+        /// <returns></returns>
+        static int sort (Box box1, Box box2)
+        {
+            if (box1==box2)
+            {
+                return 0;
+            }
+            else if (box1>=box2)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        } 
+    }
+}
